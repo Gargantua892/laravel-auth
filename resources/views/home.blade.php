4f8e679fe5a9fce1500576402e0588878d6fa8cb
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-07-20 23:43:56 +0200

    titolo corretto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,18 @@
         <div class="row">
             @foreach ($projects as $project)
                 <div class="col-lg-4">
-                    ciao
+                    <div class="card mb-3" style="max-width: 540px;">
+                        <div class="row g-0">
+                          <div class="col-md-4">
+                            <img src="{{$project -> thumb}}" class="img-fluid rounded-start" alt="Card title">
+                          </div>
+                          <div class="col-md-8">
+                            <div class="card-body">
+                              <h5 class="card-title">{{$project -> title}}</h5>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                 </div>
             @endforeach
         </div>
